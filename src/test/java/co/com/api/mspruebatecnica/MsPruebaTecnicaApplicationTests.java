9e9c60b74d95b49d09e9e8a507358c96cebb41c9
USUARIO <jorgee.vargac@utadeo.edu.co>
2023-01-04 11:15:24 -0500

    modificado properties y pruebas unitarias

--- src/test/java/co/com/api/mspruebatecnica/MsPruebaTecnicaApplicationTests.java
+++ src/test/java/co/com/api/mspruebatecnica/MsPruebaTecnicaApplicationTests.java
@@ -3,15 +3,11 @@ package co.com.api.mspruebatecnica;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.datasource.url=jdbc:mysql://localhost:3306/sistema_transaccional?createDatabaseIfNotExist=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"})
 class MsPruebaTecnicaApplicationTests {
-
-	@Test
-	void main( ) {
-		MsPruebaTecnicaApplication.main(new String[] {});
-		Assertions.assertTrue(true, "assertion");
-	}
 
 	@Test
 	void contextLoads() {
--- src/test/java/co/com/api/mspruebatecnica/controller/ReporteControllerTest.java
+++ src/test/java/co/com/api/mspruebatecnica/controller/ReporteControllerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.datasource.url=jdbc:mysql://localhost:3306/sistema_transaccional?createDatabaseIfNotExist=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"})
 class ReporteControllerTest {
 
     @Autowired
